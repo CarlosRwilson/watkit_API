da433f0776f48dbcfeb9b605c290d756976af470
Wilson23MLPro <wilsoncarlosraul@gmail.com>
2026-02-16 17:10:59 -0800

    agregacion de nuevas tablas en models

--- app/infraestructure/database/models.py
+++ app/infraestructure/database/models.py
@@ -1,10 +1,13 @@
-from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
-from sqlalchemy import String, DateTime, func
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column,relationship
+from sqlalchemy import String, DateTime, func, ForeignKey
 from datetime import datetime
+from typing import List
+from sqlalchemy import Float, Text, Boolean
 
 
 class Base(DeclarativeBase):
     pass
+
 
 class Client(Base):
     __tablename__ = "clients"
@@ -15,5 +18,29 @@ class Client(Base):
     register_date: Mapped[datetime] = mapped_column(server_default=func.now()) #time created
     last_message: Mapped[datetime | None] = mapped_column(onupdate=func.now())#time updated
     status : Mapped[str] = mapped_column(default="begin")
+    #foregein key to connect to enterprise
+    enterprise_id: Mapped[int] = mapped_column(ForeignKey("enterprises.id"))
+    enterprise: Mapped["Enterprise"] = relationship(back_populates="clients")
 
-    
+
+class Enterprise(Base):
+    __tablename__ = "enterprises"
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(20))
+    api_key: Mapped[str] = mapped_column(String(100), unique=True) 
+    whatsapp_number: Mapped[str] = mapped_column(String(100), unique=True)
+
+    #one enterprise has a lot of clients
+    clients: Mapped[list["Client"]] = relationship(back_populates="enterprise")
+
+class Product(Base):
+    __tablename__ = 'products'
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(80))
+    description : Mapped[str] = mapped_column(String(100))
+    price: Mapped[float] = mapped_column(Float(10,00))
+    #default true if Product exist, otherwise change to false
+    activo: Mapped[bool] = mapped_column(Boolean, default=True)
+
+    enterprise_id: Mapped[int] = mapped_column(ForeignKey("enterprises.id"))
+    enterprise: Mapped["Enterprise"] = relationship(back_populates="products")
