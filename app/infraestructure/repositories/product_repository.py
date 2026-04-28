5c6b9db74f4d825bac5e585b27f9681c863642f1
Carlos Wilson <wilsoncarlosraul@gmail.com>
2026-04-28 14:10:39 -0700

    Update business logic layer comment in product_repository

--- app/infraestructure/repositories/product_repository.py
+++ app/infraestructure/repositories/product_repository.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from sqlalchemy import update
 from app.infraestructure.database.models import Product
-#Logic business layer 1
+#Logic business layer 2
 class ProductRepository:
     def __init__(self, session: AsyncSession):
         self.session = session
